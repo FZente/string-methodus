97a4d4192f8710e8d229fb1134ad1a19b80e18ec
FZente <98951593+FZente@users.noreply.github.com>
2024-10-08 08:12:13 +0200

    Add files via upload

---
+++ stringmethods.js
@@ -0,0 +1,17 @@
+let szoveg = "Valami nincs itt rendebn és nem tudom, hogy mit írok, csak azt írom, ami az eszembe jut, ennek a szövegnek nincsen semmilyen értelme, sem haszna. Nem tudom, hogy mi fog még az eszembe jutni, de szerintem ez egy elég hosszú szöveg már."
+
+console.log(szoveg.slice(0,5));
+console.log();
+console.log(szoveg.slice(3,8)); 
+console.log();
+console.log(szoveg.slice(5, szoveg.length));
+console.log();
+console.log(szoveg.slice(5, 5+6));
+console.log();
+console.log(szoveg.toUpperCase());
+console.log();
+console.log();
+console.log();
+console.log(szoveg.replace("e", "E"));
+console.log();
+console.log(szoveg.replaceAll("e", " "));
